ed26b6a30faeed32b76d94d2936281b8d1d62efb
Mukul Kumar <mukul.2121@gmail.com>
2019-04-12 02:24:27 +0530

    Added Icon

---
+++ app/src/main/java/com/example/agoravoteprototype2/Election1.java
@@ -0,0 +1,13 @@
+package com.example.agoravoteprototype2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Election1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_election1);
+    }
+}
